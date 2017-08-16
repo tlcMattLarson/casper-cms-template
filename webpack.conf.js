d8c2f3b68601b413b26ce921c011267302683f4b
tlcMattLarson <30934703+tlcMattLarson@users.noreply.github.com>
2017-08-16 17:16:10 -0500

    fixes

--- webpack.conf.js
+++ webpack.conf.js
@@ -10,7 +10,7 @@ export default {
       },
       {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel-loader",
+        loader: "babel",
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
